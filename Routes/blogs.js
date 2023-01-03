255196c3a59babdae3c2ec994d904a995c6c87c4
jatin choudhary <73272963+jatinmark@users.noreply.github.com>
2023-01-03 10:36:13 +0530

    Revert "adding blogs api"

--- Routes/blogs.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const {postblogs , getblogs} = require('../Controllers/blogs')
-
-router.post("/postblogs", postblogs);
-router.get("/getblogs",  getblogs);
-
-module.exports = router;
